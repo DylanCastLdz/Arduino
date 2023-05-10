756c6643587b0852b6ee866e20fe51074553f1a9
Dylan <albert.castldz@gmail.com>
2023-05-09 18:51:43 -0600

    feat(comentarios): Practica 5
    Agregando comentarios al código de la práctica
    05-Indicador_de_estado_de_Animo

--- 05-Indicador_de_estado_de_Animo/05-Indicador_de_estado_de_Animo.ino
+++ 05-Indicador_de_estado_de_Animo/05-Indicador_de_estado_de_Animo.ino
@@ -1,24 +1,26 @@
-#include <Servo.h>
-Servo myServo;
+#include <Servo.h> // Incluyendo la libreria Servo.H
+Servo myServo; // Creando el objetos "MyServo"
 
-const int potPin = A0;
-int potVal = 0;
-int angulo = 0;
+const int potPin = A0; // Asignando entrada analogica
+int potVal = 0; // Variable para almacenar el valor del potenciometro
+int angulo = 0; // Variable para almacenar el valor del ángulo
 
 void setup () {
-  myServo.attach(9);
-  Serial.begin(9600);
+	myServo.attach(9); //Asignando salida del objeto "MyServo"
+	Serial.begin(9600); //Inicializando puerto serial
 }
 
 void loop () {
-  potVal = analogRead(potPin);
-  Serial.print("Valor del Potenciometro: ");
-  Serial.print(potVal);
+	potVal = analogRead(potPin); //Leyendo potenciometro y almacenado en variable
+	// Imprimiendo valores del potenciometro
+	Serial.print("Valor del Potenciometro: "); 
+	Serial.print(potVal);
 
-  angulo = map(potVal, 0, 1023, 0, 179);
-  Serial.print(" || Angulo: ");
-  Serial.println(angulo);
+	angulo = map(potVal, 0, 1023, 0, 179); // Convirtiendo valores del potenciometro a angulo válido para servo
+	// Imprimiendo valores del angulo del servo
+	Serial.print(" || Angulo: ");
+	Serial.println(angulo);
 
-  myServo.write(angulo);
-  delay(15);
-}
+	// Enviando señal de movimiento al servo
+	myServo.write(angulo);
+	delay(15);
