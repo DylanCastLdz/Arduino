63bcb78b44f907330caba28d02edab984615f796
Dylan <albert.castldz@gmail.com>
2023-05-20 20:45:34 -0600

    feat(monitor serial): Muestra valores del sensor
    Se inicializa el monitor serial, y se imprimen los valores del sensor y
    del pitch para confirmar que el mapeo se ejecute correctamente

--- 06-Theremin_Optico/06-Theremin_Optico.ino
+++ 06-Theremin_Optico/06-Theremin_Optico.ino
@@ -5,6 +5,8 @@ int sensorHigh = 0;
 const int ledPin = 13;
 
 void setup() {
+  Serial.begin(9600);
+
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
 
@@ -26,7 +28,11 @@ void loop() {
 
   int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 4000);
 
-  tone(8, pitch, 20);
+  Serial.print("sensorValue: ");
+  Serial.print(sensorValue);
+  Serial.print(" || Pitch: ");
+  Serial.println(pitch);
 
+  tone(8, pitch, 20);
   delay(10);
 }
