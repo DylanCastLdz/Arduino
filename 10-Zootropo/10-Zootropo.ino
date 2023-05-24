2e2d7eaebb5c93c820be15a6b8b584b9842db937
Dylan <albert.castldz@gmail.com>
2023-05-23 18:09:01 -0600

    feat(practica): Proyecto 10
    add | 10-Zootopo.ino

--- 10-Zootropo/10-Zootropo.ino
+++ 10-Zootropo/10-Zootropo.ino
@@ -1,63 +1,64 @@
-const int controlPinL = 2;
-const int controlPinR = 3;
-const int velPin = 9;
-const int directionSwitchPin = 4;
-const int powerPin = 5;
-const int potPin = A0;
+// Declarando constantes para clavijas
+// unoA y dosA controlan la dirección del giro
+const int unoA = 2;
+const int dosA = 3;
+const int en1 = 9;  // enciende y apaga el motor, así como suministra PWM al L293D
+const int inverterPuls = 4;  // recibe la señal del pulsador de cambio de dirección
+const int powerPuls = 5;  // recibe la señal del pulsador de encendido y apagado
+const int potPin = A0;  // recibe la señal del potenciometro de giro
 
-int powerState = 0;
-int previousPowerState = 0;
-int directionState = 0;
-int previousDirectionState = 0;
+int powerState = 0;  // para almacenar el estado del pulsador de power
+int previousPowerState = 0;  // para almacenar una copia del estado actual del pulsador antes de actualizarlo
 
-int motorEnable = 0;
-int motorSpeed = 0;
-int motorDirection = 1;
+int directionState = 0;  // para almacenat el sentido actual de giro
+int previousDirectionState = 0;  // para almacenar una copia del sentido actual de giro antes de actualizarlo
+
+int motorSpeed = 0;  // para registrar la velocidad
+int motorEnable = 0;  // para controlar el encendido y apagado
+int motorDirection = 1;  // para registrar en qué dirección gira el motor
 
 void setup() {
-  pinMode(directionSwitchPin, INPUT);
-  pinMode(powerPin, INPUT);
-  pinMode(controlPinL, OUTPUT);
-  pinMode(controlPinR, OUTPUT);
-  pinMode(powerPin, OUTPUT);
+  pinMode(unoA, OUTPUT);
+  pinMode(dosA, OUTPUT);
+  pinMode(en1, OUTPUT);
+  pinMode(inverterPuls, INPUT);
+  pinMode(powerPuls, INPUT);
 
-  digitalWrite(velPin, LOW);
+  digitalWrite(en1, LOW);
 }
 
 void loop() {
-  powerState = digitalRead(powerPin);
+  powerState = digitalRead(powerPuls);
   delay(1);
-
-  directionState = digitalRead(directionSwitchPin);
-
+  directionState = digitalRead(inverterPuls);
   motorSpeed = analogRead(potPin)/4;
 
   if (powerState != previousPowerState) {
     if (powerState == HIGH) {
-      motorEnable =! motorEnable;
+      motorEnable = !motorEnable;
     }
   }
 
   if (directionState != previousDirectionState) {
     if (directionState == HIGH) {
-      motorDirection =! motorDirection;
+      motorDirection = !motorDirection;
     }
   }
 
   if (motorDirection == 1) {
-    digitalWrite(controlPinL, HIGH);
-    digitalWrite(controlPinR, LOW);
+    digitalWrite(unoA, HIGH);
+    digitalWrite(dosA, LOW);
   }
   else {
-    digitalWrite(controlPinL, LOW);
-    digitalWrite(controlPinR, HIGH);
+    digitalWrite(unoA, LOW);
+    digitalWrite(dosA, HIGH);
   }
 
   if (motorEnable == 1) {
-    analogWrite(velPin, motorSpeed);
+    analogWrite(en1, motorSpeed);
   }
   else {
-    analogWrite(velPin, 0);
+    analogWrite(en1, 0);
   }
 
   previousDirectionState = directionState;
